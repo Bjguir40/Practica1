45fe6cebba9928cd507aeffc188181be3109fcca
Byron G <Byron G@Byron>
2017-08-12 22:50:02 -0600

    comienzo1

---
+++ src/practica1/menu.java
@@ -0,0 +1,31 @@
+package practica1;
+/**
+ *
+ * @author Byron G
+ */
+
+public class menu {
+
+
+    void menu1() {
+ 
+        System.out.println("  Byron Josué Güir González");
+        System.out.println("         201602511");
+        System.out.println("      IPC1 A PRACTICA1");
+        System.out.println("       ! BUSCAMINAS !");
+
+       
+             System.out.println("   ! BUSCAMINAS !");
+             System.out.println("1. principiante");
+             System.out.println("2. intermedio");
+             System.out.println("3. experto");
+             System.out.println("4. salir");
+
+  }
+}
+
+    
+      
+    
+    
+
